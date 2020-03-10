8686a003274d8e3d18c8829834209ff5fd573a0f
Mondal10 <amitmondal111.am@gmail.com>
2020-03-10 18:12:25 +0530

    desktop capturer getting video source list and generated menu popup

--- src/render.js
+++ src/render.js
@@ -1,1 +1,23 @@
-console.log('renderer loaded');
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote; // To make menu-popup
+
+const videoSelectionBtn = document.getElementById('video-selection-btn');
+videoSelectionBtn.addEventListener('click', getVideoSources);
+
+// Get all the available video sources
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            }
+        })
+    );
+
+    videoOptionsMenu.popup();
+}
